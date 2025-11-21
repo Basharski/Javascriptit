ae206adc766e5665d35bc3981621413f7a66646c
bashara-del <bashar.alsayedali@metropolia.fi>
2025-11-21 10:04:43 +0200

    yurrr

---
+++ Moduuli_1/1_4/1.4.js
@@ -0,0 +1,18 @@
+
+
+const studentName = prompt("Enter student's name:");
+const randomNumber = Math.floor(Math.random() * 4) + 1;
+
+let house;
+
+if (randomNumber === 1) {
+  house = "Gryffindor";
+} else if (randomNumber === 2) {
+  house = "Slytherin";
+} else if (randomNumber === 3) {
+  house = "Hufflepuff";
+} else {
+  house = "Ravenclaw";
+}
+
+document.body.innerHTML = `${studentName}, you are ${house}.`;
