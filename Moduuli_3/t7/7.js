52f0497268a65fbfb541e658a9cd3651c42f88a5
bashara-del <bashar.alsayedali@metropolia.fi>
2025-12-02 12:02:07 +0200

    t7 fix

--- Moduuli_3/t7/7.js
+++ Moduuli_3/t7/7.js
@@ -3,8 +3,8 @@
 const trigger = document.getElementById('trigger');
 const image = document.getElementById('target');
 
-const originalSrc = 'picA.jpg';
-const hoverSrc = 'picB.jpg';
+const originalSrc = 'img/picA.jpg';
+const hoverSrc = 'img/picB.jpg';
 
 trigger.addEventListener('mouseover', function () {
   image.src = hoverSrc;
